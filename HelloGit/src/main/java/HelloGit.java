e2fc74635a6dd3aaa1d5ca0f693c8393fa60732b
tang <tang@test.com>
2020-07-20 10:30:30 +0800

    frist commit

---
+++ HelloGit/src/main/java/HelloGit.java
@@ -0,0 +1,9 @@
+/**
+ * @author Tangll
+ * @create 2020-07-20上午 10:12
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
